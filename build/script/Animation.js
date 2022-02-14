3138a127d2b72e947d56f56a7296874a9591455b
Afanasiev Alexander <jei12061988@yandex.ru>
2022-02-14 12:35:00 +0500

    refactoring

--- build/script/Animation.js
+++ build/script/Animation.js
@@ -1,10 +1,11 @@
 class Animation {
-  constructor(type, elem, path, duration, delay, setUpdate, onEnd) {
+  constructor(type, elem, path, duration, delay, renderable, setUpdate, onEnd) {
     this._type = type;
     this._elem = elem;
     this._path = path;
     this._duration = duration;
     this._delay = delay || 0;
+    this._renderable = renderable;
     this._setUpdate = setUpdate;
     this.onEnd = onEnd || null;
 
@@ -29,42 +30,40 @@ class Animation {
     return this._type;
   }
 
+  getRenderable() {
+    return this._renderable;
+  }
+
   start(timestamp) {
     this._startTime = timestamp;
     this._running = true;
   }
 
   update(timestamp) {
-    let timeProgress = this._getTimeProgress(timestamp);
-    timeProgress = timeProgress > 100 ? 100 : timeProgress;
+    const timeProgress = this._getTimeProgress(timestamp);
     const pathProgress = this._getPathProgress(timeProgress);
     const { currentStep, nextStep, currentStepProgress } = pathProgress;
-    
     this._setUpdate(this._elem, currentStep, nextStep, currentStepProgress);
-
     this._ended = timeProgress === 100;
   }
 
   _getPathProgress(timeProgress) {
     const pathProgress =  timeProgress / 100 * (this._path.length - 1);
     const currentStep = this._path[Math.floor(pathProgress)];
-    const nextStep =  Math.floor(pathProgress) === this._path.length - 1 ? currentStep : this._path[Math.floor(pathProgress) + 1];
+    const nextStep = Math.floor(pathProgress) === this._path.length - 1 ? currentStep : this._path[Math.floor(pathProgress) + 1];
     const currentStepProgress = pathProgress - Math.floor(pathProgress);
-    return {
-      currentStep,
-      nextStep,
-      currentStepProgress
-    }
+    return { currentStep, nextStep, currentStepProgress };
   }
 
   _getTimeProgress(timestamp) {
     const progress = (timestamp - this._startTime - this._delay) / this._duration * 100;
-    return progress < 0 ? 0 : progress;
+    return progress < 0 ? 0 : progress > 100 ? 100 : progress;
   }
 }
 
 Animation.TYPES = [
   'ball move',
   'ball spawn',
-  'ball destroy'
+  'ball destroy',
+  'ball prepare'
 ]
